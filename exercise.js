6d338756a512b45f9a908afb1fb94be364ea2cee
xAlessandroo <alessandro.iozzino1@gmail.com>
2023-11-16 12:10:13 +0100

    modifica esercizio

--- exercise.js
+++ exercise.js
@@ -2,7 +2,9 @@ async function getTodoList(){
     const link = "https://jsonplaceholder.typicode.com/todos";
     const response = await fetch(link);
     const data = await response.json()
-    console.log(data);
+    return data;
 }
 
 getTodoList()
+.then ((val) => console.log(val))
+.catch ((err) => console.error(err))
